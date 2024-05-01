af6579374a4277d9f4e2e3f2878a6ea8affb8eba
Timur0798 <162792932+Timur0798@users.noreply.github.com>
2024-05-01 21:49:42 +0300

    Changes on 01/05/2024

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
 
 # Create your models here.
+class Author(models.Model):
+    authorUser=models.OneToOneField(User,on_delete=models.CASCADE)
+    authorRating=models.IntegerField(default=0)
+    def update_rating(self):
+        sumRating=self.post_set.aggregate(postRating=Sum('newsRating'))
+        pRat = 0
+        pRat += sumRating.get('postRating')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64,unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+
+    Article = 'AR'
+    News = 'NW'
+    CATEGORY_CHOICES = (
+        (Article, 'Статья'),
+        (News, 'Новость')
+    )
+    categoryType = models.CharField(max_length=2,choices=CATEGORY_CHOICES,default=Article)
+    creationTime = models.DateTimeField(auto_now_add=True)
+    newsCategory = models.ManyToManyField(Category,through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    newsRating = models.IntegerField(default=0)
+
+    def like(self):
+        self.newsRating += 1
+        self.save()
+
+    def dislike(self):
+        self.newsRating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[0:123] + '...'
+
+
+class PostCategory(models.Model):
+    postThrough = models.ForeignKey(Post,on_delete=models.CASCADE)
+    categoryThrough = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post,on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User,on_delete=models.CASCADE)
+    commentCreation = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    commentRating = models.IntegerField(default=0)
+
+
+    def like(self):
+        self.commentRating += 1
+        self.save()
+
+    def dislike(self):
+        self.commentRating -= 1
+        self.save()
